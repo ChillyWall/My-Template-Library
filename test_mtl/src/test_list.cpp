efada7389d387c5d38291e9671c165c1bebe587d
Chilly <chillywall@163.com>
2025-02-18 18:14:08 +0800

    complete test_list

--- test_mtl/src/test_list.cpp
+++ test_mtl/src/test_list.cpp
@@ -14,8 +14,21 @@ TEST(TestListConstructor, TestListDefaultConstructor) {
 TEST(TestConstructor, TestListInitListConstructor) {
     list<int> l = {1, 2, 3};
     EXPECT_EQ(l.size(), 3);
+    auto itr = l.begin();
     for (int i = 1; i <= 3; ++i) {
-        EXPECT_EQ(l.front(), i);
+        EXPECT_EQ(*itr, i);
+        ++itr;
+    }
+}
+
+TEST(TestListConstructor, TestListCopyConstructor) {
+    list<int> l1 = {1, 2, 3};
+    list<int> l2(l1);
+    EXPECT_EQ(l2.size(), 3);
+    auto itr = l2.begin();
+    for (int i = 1; i <= 3; ++i) {
+        EXPECT_EQ(*itr, i);
+        ++itr;
     }
 }
 
@@ -24,21 +37,94 @@ TEST(TestListOperation, TestListPushBack) {
     l.push_back(1);
     l.push_back(2);
     l.push_back(3);
+    auto itr = l.begin();
     for (int i = 1; i <= 3; ++i) {
-        EXPECT_EQ(l.front(), i);
-        l.pop_front();
+        EXPECT_EQ(*itr, i);
+        ++itr;
     }
 }
 
-TEST(TestListConstructor, TestListCopyConstructor) {
-    list<int> l1;
-    l1.push_back(1);
-    l1.push_back(2);
-    l1.push_back(3);
-    list<int> l2(l1);
-    EXPECT_EQ(l2.size(), 3);
-    EXPECT_EQ(l2.front(), 1);
-    EXPECT_EQ(l2.back(), 3);
+TEST(TestListOperation, TestListPushFront) {
+    list<int> l;
+    l.push_front(3);
+    l.push_front(2);
+    l.push_front(1);
+    auto itr = l.begin();
+    for (int i = 1; i <= 3; ++i) {
+        EXPECT_EQ(*itr, i);
+        ++itr;
+    }
+}
+
+TEST(TestListOperation, TestListPopBack) {
+    list<int> l = {1, 2, 3};
+    l.pop_back();
+    EXPECT_EQ(l.size(), 2);
+    auto itr = l.begin();
+    EXPECT_EQ(*itr, 1);
+    ++itr;
+    EXPECT_EQ(*itr, 2);
+}
+
+TEST(TestListOperation, TestListPopFront) {
+    list<int> l = {1, 2, 3};
+    l.pop_front();
+    EXPECT_EQ(l.size(), 2);
+    auto itr = l.begin();
+    EXPECT_EQ(*itr, 2);
+    ++itr;
+    EXPECT_EQ(*itr, 3);
+}
+
+TEST(TestListOperation, TestListInsert) {
+    list<int> l = {1, 2, 3};
+    auto itr = l.begin();
+    ++itr;
+    l.insert(itr, 4);
+    EXPECT_EQ(l.size(), 4);
+    itr = l.begin();
+    EXPECT_EQ(*itr, 1);
+    ++itr;
+    EXPECT_EQ(*itr, 4);
+    ++itr;
+    EXPECT_EQ(*itr, 2);
+    ++itr;
+    EXPECT_EQ(*itr, 3);
+}
+
+TEST(TestListOperation, TestListRemove) {
+    list<int> l = {1, 2, 3};
+    auto itr = l.begin();
+    ++itr;
+    l.remove(itr);
+    EXPECT_EQ(l.size(), 2);
+    itr = l.begin();
+    EXPECT_EQ(*itr, 1);
+    ++itr;
+    EXPECT_EQ(*itr, 3);
+}
+
+TEST(TestListOperation, TestListRemoveRange) {
+    list<int> l = {1, 2, 3, 4, 5};
+    l.remove(l.begin() + 1, l.begin() + 3);
+    EXPECT_EQ(l.size(), 3);
+    auto itr = l.begin();
+    EXPECT_EQ(*itr, 1);
+    ++itr;
+    EXPECT_EQ(*itr, 4);
+    ++itr;
+    EXPECT_EQ(*itr, 5);
+}
+
+TEST(TestListOperation, TestListInsertRange) {
+    list<int> l = {1, 2, 3};
+    list<int> l2 = {4, 5};
+    l.insert(l.end(), l2.begin(), l2.end());
+    EXPECT_EQ(l.size(), 5);
+    auto itr = l.begin();
+    for (int i = 1; i <= 5; ++i) {
+        EXPECT_EQ(*(itr++), i);
+    }
 }
 
 int main() {
